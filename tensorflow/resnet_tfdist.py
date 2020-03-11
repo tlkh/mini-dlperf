0f957dae6178f60ac36e69d3d61e45dd4063082b
Timothy Liu <tlkh.xms@gmail.com>
2020-03-11 21:38:24 +0000

    Update NVStatsRecorder

--- tensorflow/resnet_tfdist.py
+++ tensorflow/resnet_tfdist.py
@@ -50,7 +50,7 @@ import time
 import numpy as np
 import tensorflow as tf
 import tensorflow_datasets as tfds
-from common import dataloaders, cnn_models, callbacks, schedules, optimizers
+from common import dataloaders, cnn_models, callbacks, schedules
 
 if args.stats:
     from nvstatsrecorder.callbacks import NVStats, NVLinkStats
@@ -233,7 +233,7 @@ callbacks = [time_callback, checkpoints]
 
 if args.stats:
     SUDO_PASSWORD = os.environ["SUDO_PASSWORD"]
-    nv_stats = NVStats(gpu_index=0, interval=5)
+    nv_stats = NVStats(gpu_index=0, interval=5, tensor_util=True, sudo_password=SUDO_PASSWORD)
     nvlink_stats = NVLinkStats(SUDO_PASSWORD, gpus=[0,1,2,3], interval=5)
     callbacks.append(nv_stats)
     callbacks.append(nvlink_stats)
